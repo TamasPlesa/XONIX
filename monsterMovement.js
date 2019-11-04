6f5c50a5af30dd8b5bb90f960beb6605519b0c94
Dora Molnar <molnaardora@gmail.com>
2019-11-04 21:35:41 +0100

    change spaces to numbers

--- monsterMovement.js
+++ monsterMovement.js
@@ -7,7 +7,7 @@ const monsterMovement = (arrayOfMonsters, matrix) => {
     };
     monster.x += Number(monster.xd);
     monster.y += Number(monster.yd);
-    matrix[monster.x][monster.y] = ' ';
+    matrix[monster.x][monster.y] = '3';
     matrix[temp.x][temp.y] = 1;
     if (matrix[monster.x - 1][monster.y] === 0 || matrix[monster.x + 1][monster.y] === 0) {
       monster.xd *= -1;
