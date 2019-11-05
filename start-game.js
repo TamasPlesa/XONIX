5785db377d379d22ae1edc66fb3b8cd580c56859
Mihaly Dombi <dombimihaly89@gmail.com>
2019-11-05 22:29:50 +0100

    movement update

--- start-game.js
+++ start-game.js
@@ -10,6 +10,7 @@ const arrayOfMonsters = initPositionsOfCharacters.spawnInnerBalls(gameBoard, sta
 const outerMonster = initPositionsOfCharacters.spawnOuterBall(gameBoard);
 let player = initPositionsOfCharacters.spawnPlayer(gameBoard);
 let lastPressedKey = '';
+let temporaryField = 0;
 
 const index = () => {
   gameBoard = monsterMovement.monsterMovement(arrayOfMonsters, gameBoard);
@@ -29,49 +30,56 @@ process.stdin.on('data', (key) => {
     if (player.xPosition !== 25) player = heroMovement.moveDown(player, gameBoard);
     lastPressedKey = 's';
     const firstSpaceToCut = heroMovement.findTheFirstSpace(gameBoard);
-    if (heroMovement.temporaryField === 0 && firstSpaceToCut != null) {
+    temporaryField = heroMovement.temporaryFieldAdder();
+    if (temporaryField === 0 && firstSpaceToCut != null) {
       const firstOneToCut = heroMovement.checkingSidesDown(gameBoard, firstSpaceToCut);
       heroMovement.cuttingOutSpaces(gameBoard, lastPressedKey, firstSpaceToCut);
       if (firstOneToCut != null) heroMovement.cuttingOutOnes(gameBoard, lastPressedKey, firstOneToCut);
       console.clear();
       console.log(gameBoard.join('\n'));
+      console.log('first: ', firstOneToCut);
     }
   }
   if (key === 'w') {
     if (player.xPosition !== 0) player = heroMovement.moveUp(player, gameBoard);
     lastPressedKey = 'w';
     const firstSpaceToCut = heroMovement.findTheFirstSpace(gameBoard);
-    if (heroMovement.temporaryField === 0 && firstSpaceToCut != null) {
+    temporaryField = heroMovement.temporaryFieldAdder();
+    if (temporaryField === 0 && firstSpaceToCut != null) {
       const firstOneToCut = heroMovement.checkingSidesDown(gameBoard, firstSpaceToCut);
       heroMovement.cuttingOutSpaces(gameBoard, lastPressedKey, firstSpaceToCut);
       if (firstOneToCut != null) heroMovement.cuttingOutOnes(gameBoard, lastPressedKey, firstOneToCut);
       console.clear();
       console.log(gameBoard.join('\n'));
+      console.log('first: ', firstOneToCut);
     }
   }
   if (key === 'd') {
     if (player.yPosition !== 62) player = heroMovement.moveRight(player, gameBoard);
     lastPressedKey = 'd';
     const firstSpaceToCut = heroMovement.findTheFirstSpace(gameBoard);
-    if (heroMovement.temporaryField === 0 && firstSpaceToCut != null) {
+    temporaryField = heroMovement.temporaryFieldAdder();
+    if (temporaryField === 0 && firstSpaceToCut != null) {
       const firstOneToCut = heroMovement.checkingSidesDown(gameBoard, firstSpaceToCut);
       heroMovement.cuttingOutSpaces(gameBoard, lastPressedKey, firstSpaceToCut);
       if (firstOneToCut != null) { heroMovement.cuttingOutOnes(gameBoard, lastPressedKey, firstOneToCut); }
       console.clear();
       console.log(gameBoard.join('\n'));
+      console.log('first: ', firstOneToCut);
     }
   }
   if (key === 'a') {
     if (player.yPosition !== 0) player = heroMovement.moveLeft(player, gameBoard);
     lastPressedKey = 'a';
     const firstSpaceToCut = heroMovement.findTheFirstSpace(gameBoard);
-    if (heroMovement.temporaryField === 0 && firstSpaceToCut != null) {
+    temporaryField = heroMovement.temporaryFieldAdder();
+    if (temporaryField === 0 && firstSpaceToCut != null) {
       const firstOneToCut = heroMovement.checkingSidesDown(gameBoard, firstSpaceToCut);
       heroMovement.cuttingOutSpaces(gameBoard, lastPressedKey, firstSpaceToCut);
-      console.log(firstOneToCut);
       if (firstOneToCut != null) { heroMovement.cuttingOutOnes(gameBoard, lastPressedKey, firstOneToCut); }
       console.clear();
       console.log(gameBoard.join('\n'));
+      console.log('first: ', firstOneToCut);
     }
   }
   if (key === 'q') {
