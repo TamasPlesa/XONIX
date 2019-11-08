e608947d3ca28fd44f6c17d0eb19a468d4ba5490
Mihaly Dombi <dombimihaly89@gmail.com>
2019-11-08 14:52:07 +0100

    vasd

--- monsterMovement.js
+++ monsterMovement.js
@@ -13,10 +13,10 @@ const monsterMovement = (arrayOfMonsters, matrix) => {
       monster.yPosition += Number(monster.yDirection);
       matrix[monster.xPosition][monster.yPosition] = 3;
       matrix[temp.x][temp.y] = 1;
-      if (matrix[monster.xPosition - 1][monster.yPosition] === 0 || matrix[monster.xPosition + 1][monster.yPosition] === 0 || matrix[monster.xPosition - 1][monster.yPosition] === 4 || matrix[monster.xPosition + 1][monster.yPosition] === 4 || matrix[monster.xPosition - 1][monster.yPosition] === 2 || matrix[monster.xPosition + 1][monster.yPosition] === 2) {
+      if (matrix[monster.xPosition - 1][monster.yPosition] === 0 || matrix[monster.xPosition + 1][monster.yPosition] === 0 || matrix[monster.xPosition - 1][monster.yPosition] === 4 || matrix[monster.xPosition + 1][monster.yPosition] === 4) {
         monster.xDirection *= -1;
       }
-      if (matrix[monster.xPosition][monster.yPosition - 1] === 0 || matrix[monster.xPosition][monster.yPosition + 1] === 0 || matrix[monster.xPosition][monster.yPosition - 1] === 4 || matrix[monster.xPosition][monster.yPosition + 1] === 4 || matrix[monster.xPosition][monster.yPosition - 1] === 2 || matrix[monster.xPosition][monster.yPosition + 1] === 2) {
+      if (matrix[monster.xPosition][monster.yPosition - 1] === 0 || matrix[monster.xPosition][monster.yPosition + 1] === 0 || matrix[monster.xPosition][monster.yPosition - 1] === 4 || matrix[monster.xPosition][monster.yPosition + 1] === 4) {
         monster.yDirection *= -1;
       }
     }
